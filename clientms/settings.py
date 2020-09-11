de9c55826f8035f3ba91b38353b649eb131b9be8
James Franco <Jdfranco@unomaha.edu>
2020-09-11 16:09:24 -0500

    Login/Logout Redirect Replaced

--- clientms/settings.py
+++ clientms/settings.py
@@ -133,6 +133,9 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL ='/media/'
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
